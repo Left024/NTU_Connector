0756c30eebc07ceada2f69d3f558cd900734e06a
Left024 <left024@gmail.com>
2020-12-13 16:13:39 +0800

    完成网络连接检查

--- NTU/Form1.cs
+++ NTU/Form1.cs
@@ -12,6 +12,10 @@ using SimpleWifi;
 using System.Net;
 using Thread = System.Threading.Thread;
 using System.Net.NetworkInformation;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+using System.Net.Sockets;
+using System.IO;
 
 namespace NTU
 {
@@ -36,6 +40,7 @@ namespace NTU
             public static string password;
             public static string url;
             public static string ip;
+            public static string logout;
         }
 
         Wifi g_wifi;
@@ -255,8 +260,7 @@ namespace NTU
             else
             {
                 string name = Dns.GetHostName();
-                IPAddress[] ipadrlist = Dns.GetHostAddresses(name);
-                CommonData.ip = ipadrlist[1].ToString();
+                CommonData.ip = GetLocalIP();
                 if (xyw.Checked == true)
                 {
                     CommonData.yys = "";
@@ -275,7 +279,7 @@ namespace NTU
                 }
                 CommonData.username = UsernameTextBox.Text;
                 CommonData.password = PasswordTextBox.Text;
-                CommonData.url = "http://210.29.79.141:801/eportal/?c=Portal&a=login&callback=dr1003&login_method=1&user_account=%2C0%2C" + CommonData.username + CommonData.yys + "&user_password=" + CommonData.password + "&wlan_user_ip=" + CommonData.ip + "&wlan_user_ipv6=&wlan_user_mac=000000000000&wlan_ac_ip=&wlan_ac_name=ME60&jsVersion=3.3.2&v=5722";
+                CommonData.url = "http://210.29.79.141:801/eportal/?c=Portal&a=login&callback=dr1003&login_method=1&user_account=%2C0%2C" + CommonData.username + CommonData.yys + "&user_password=" + CommonData.password + "&wlan_user_ip=" + CommonData.ip + "&wlan_user_ipv6=&wlan_user_mac=000000000000&wlan_ac_ip=&wlan_ac_name=ME60&jsVersion=3.3.2&v=6376";
                 foreach (var item in t)
                 {
                     if (item.Name == "NTU")
@@ -318,12 +322,19 @@ namespace NTU
 
         private void logout_Click(object sender, EventArgs e)
         {
-            
+            string url = string.Format("http://210.29.79.141:801/eportal/?c=Portal&a=logout&callback=dr1003&login_method=1&user_account=drcom&user_password=123&ac_logout=0&register_mode=1&wlan_user_ip="+CommonData.ip+"&wlan_user_ipv6=&wlan_vlan_id=0&wlan_user_mac=000000000000&wlan_ac_ip=&wlan_ac_name=ME60&jsVersion=3.3.2&v=4080");
+            using (var wc = new WebClient())
+            {
+                Encoding enc = Encoding.GetEncoding("UTF-8");
+                Byte[] pageData = wc.DownloadData(url);
+                string re = enc.GetString(pageData);
+            }
+            CommonData.logout = "1";
         }
 
         private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
         {
-            toolStripStatusLabel1.Text = "test";
+            
         }
 
         void bgworker_DoWork(object sender, DoWorkEventArgs e)
@@ -331,29 +342,188 @@ namespace NTU
             BackgroundWorker worker = sender as BackgroundWorker;
             for (; ; )
             {
-                int i = 0;
-                i = i + 1;
-                Thread.Sleep(100);
-                worker.ReportProgress(i);
+                Thread.Sleep(1000);
+
+                string baidu = GetHtmlByUrl("http://baidu.com/1.txt");
+                if (baidu.IndexOf("baidu") == -1)
+                {
+                    toolStripStatusLabel1.Text = "未连接";
+                }
+                else
+                {
+                    toolStripStatusLabel1.Text = "已连接";
+                }
             }
             
-            /*
-            Ping ping = new Ping();
-            PingReply pr = ping.Send("baidu.com");
-            if (pr.Status == IPStatus.Success)
+        }
+        /*
+        void bgworker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            if (e.ProgressPercentage.ToString() == "1")
             {
                 toolStripStatusLabel1.Text = "已连接";
             }
             else
             {
                 toolStripStatusLabel1.Text = "未连接";
-            }*/
+            }
+        }*/
+
+        //https://www.jb51.net/article/53657.htm
+        public static string GetHtmlByUrl(string url)
+        {
+            using (WebClient wc = new WebClient())
+            {
+                try
+                {
+                    wc.UseDefaultCredentials = true;
+                    wc.Proxy = new WebProxy();
+                    wc.Proxy.Credentials = CredentialCache.DefaultCredentials;
+                    wc.Credentials = System.Net.CredentialCache.DefaultCredentials;
+                    byte[] bt = wc.DownloadData(url);
+                    string txt = System.Text.Encoding.GetEncoding("GB2312").GetString(bt);
+                    switch (GetCharset(txt).ToUpper())
+                    {
+                        case "UTF-8":
+                            txt = System.Text.Encoding.UTF8.GetString(bt);
+                            break;
+                        case "UNICODE":
+                            txt = System.Text.Encoding.Unicode.GetString(bt);
+                            break;
+                        default:
+                            break;
+                    }
+                    return txt;
+                }
+                catch (Exception ex)
+                {
+                    return null;
+                }
+            }
         }
 
-        void bgworker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        public static string GetCharset(string html)
         {
-            int i = e.ProgressPercentage;
-            toolStripStatusLabel1.Text = e.ProgressPercentage.ToString();
+            string charset = "";
+            Regex regCharset = new Regex(@"content=[""'].*\s*charset\b\s*=\s*""?(?<charset>[^""']*)", RegexOptions.IgnoreCase);
+            if (regCharset.IsMatch(html))
+            {
+                charset = regCharset.Match(html).Groups["charset"].Value;
+            }
+            if (charset.Equals(""))
+            {
+                regCharset = new Regex(@"<\s*meta\s*charset\s*=\s*[""']?(?<charset>[^""']*)", RegexOptions.IgnoreCase);
+                if (regCharset.IsMatch(html))
+                {
+                    charset = regCharset.Match(html).Groups["charset"].Value;
+                }
+            }
+            return charset;
+        }
+
+
+
+        //https://www.cnblogs.com/lijianda/p/6604651.html
+        /// <summary>
+        /// 获取当前使用的IP
+        /// </summary>
+        /// <returns></returns>
+        public static string GetLocalIP()
+        {
+            string result = RunApp("route", "print", true);
+            Match m = Regex.Match(result, @"0.0.0.0\s+0.0.0.0\s+(\d+.\d+.\d+.\d+)\s+(\d+.\d+.\d+.\d+)");
+            if (m.Success)
+            {
+                return m.Groups[2].Value;
+            }
+            else
+            {
+                try
+                {
+                    System.Net.Sockets.TcpClient c = new System.Net.Sockets.TcpClient();
+                    c.Connect("www.baidu.com", 80);
+                    string ip = ((System.Net.IPEndPoint)c.Client.LocalEndPoint).Address.ToString();
+                    c.Close();
+                    return ip;
+                }
+                catch (Exception)
+                {
+                    return null;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 获取本机主DNS
+        /// </summary>
+        /// <returns></returns>
+        public static string GetPrimaryDNS()
+        {
+            string result = RunApp("nslookup", "", true);
+            Match m = Regex.Match(result, @"\d+\.\d+\.\d+\.\d+");
+            if (m.Success)
+            {
+                return m.Value;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// 运行一个控制台程序并返回其输出参数。
+        /// </summary>
+        /// <param name="filename">程序名</param>
+        /// <param name="arguments">输入参数</param>
+        /// <returns></returns>
+        public static string RunApp(string filename, string arguments, bool recordLog)
+        {
+            try
+            {
+                if (recordLog)
+                {
+                    Trace.WriteLine(filename + " " + arguments);
+                }
+                Process proc = new Process();
+                proc.StartInfo.FileName = filename;
+                proc.StartInfo.CreateNoWindow = true;
+                proc.StartInfo.Arguments = arguments;
+                proc.StartInfo.RedirectStandardOutput = true;
+                proc.StartInfo.UseShellExecute = false;
+                proc.Start();
+
+                using (System.IO.StreamReader sr = new System.IO.StreamReader(proc.StandardOutput.BaseStream, Encoding.Default))
+                {
+                    //string txt = sr.ReadToEnd();
+                    //sr.Close();
+                    //if (recordLog)
+                    //{
+                    //    Trace.WriteLine(txt);
+                    //}
+                    //if (!proc.HasExited)
+                    //{
+                    //    proc.Kill();
+                    //}
+                    //上面标记的是原文，下面是我自己调试错误后自行修改的
+                    Thread.Sleep(100);           //貌似调用系统的nslookup还未返回数据或者数据未编码完成，程序就已经跳过直接执行
+                                                 //txt = sr.ReadToEnd()了，导致返回的数据为空，故睡眠令硬件反应
+                    if (!proc.HasExited)         //在无参数调用nslookup后，可以继续输入命令继续操作，如果进程未停止就直接执行
+                    {                            //txt = sr.ReadToEnd()程序就在等待输入，而且又无法输入，直接掐住无法继续运行
+                        proc.Kill();
+                    }
+                    string txt = sr.ReadToEnd();
+                    sr.Close();
+                    if (recordLog)
+                        Trace.WriteLine(txt);
+                    return txt;
+                }
+            }
+            catch (Exception ex)
+            {
+                Trace.WriteLine(ex);
+                return ex.Message;
+            }
         }
     }
 }
